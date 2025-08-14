13b1ac823d9c8597deff05e625a2e94e61a9b23d
Kate Popova <popova1919@yandex.ru>
2025-08-14 20:37:19 +0530

    Changed identification of date

--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -6,6 +6,8 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.visible;
@@ -18,7 +20,9 @@ class CardDeliveryTest {
     Configuration.holdBrowserOpen = true;
     open("http://localhost:9999/");
     $("[data-test-id='city'] input").setValue("Майкоп");
-    $("[data-test-id='date'] input").setValue("17.08.2025");
+    String date = LocalDate.now().plusDays(3)
+    .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    $("[data-test-id='date'] input").setValue(date);
     $("[data-test-id='name'] input").setValue("Петров Олег");
     $("[data-test-id='phone'] input").setValue("+71234567890");
     $("[data-test-id='agreement']").click();
